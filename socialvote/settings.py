0b54b1a3408bb4864f2aeee105be204c3c8005bf
Llanz Kenneth Luy <llanzkennethluy@gmail.com>
2022-09-11 14:36:40 +0800

    change project name

--- socialvote/settings.py
+++ socialvote/settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'poll.urls'
+ROOT_URLCONF = 'socialvote.urls'
 
 TEMPLATES = [
     {
@@ -73,7 +73,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'poll.wsgi.application'
+WSGI_APPLICATION = 'socialvote.wsgi.application'
 
 
 # Database
